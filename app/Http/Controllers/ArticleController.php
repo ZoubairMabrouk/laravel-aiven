fc5a1c1414b58b886b1acb02bf2bdf76be63f5ac
ZoubairMabrouk <zoubairmabrouk55@gmail.com>
2024-10-19 16:57:01 +0100

    Version 1.7

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,39 +7,45 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    public function index(){
-        $article = Article::with('scategories') ->get() ->toArray();
+    public function index()
+    {
+        $article = Article::with('scategories')->get()->toArray();
         $res = array_reverse($article);
         return response()->json($res);
     }
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $article = new Article();
-        $article->designation= $request->input('designation');
-        $article->marque= $request->input('marque');
-        $article->reference= $request->input('reference');
-        $article->qtestock= $request->input('qtestock');
-        $article->prix= $request->input('prix');
-        $article->imageart= $request->input('imageart');
-        $article->scategorieID= $request->input('scategorieID');
+        $article->designation = $request->input('designation');
+        $article->marque = $request->input('marque');
+        $article->reference = $request->input('reference');
+        $article->qtestock = $request->input('qtestock');
+        $article->prix = $request->input('prix');
+        $article->imageart = $request->input('imageart');
+        $article->scategorieID = $request->input('scategorieID');
         $article->save();
         return response()->json($article);
     }
-    public function show($id){
-        $article= Article::find($id);
+    public function show($id)
+    {
+        $article = Article::find($id);
         return response()->json($article);
     }
-    public function update($id, Request $request){
+    public function update($id, Request $request)
+    {
         $article = Article::find($id);
         $article->update($request->all());
         return response()->json($article);
-        }
-    public function destroy($id){
+    }
+    public function destroy($id)
+    {
         $article = Article::find($id);
         $article->delete();
         return response()->json(['message' => 'Article deleted successfully']);
     }
 
-    public function showArticlesPagination(Request $request){
+    public function showArticlesPagination(Request $request)
+    {
         $filtre = $request->input('filtre', '');
         $page = $request->input('page', 1);
         $pageSize = $request->input('pageSize', 10);
@@ -52,14 +58,15 @@ class ArticleController extends Controller
             ->take($pageSize)
             ->get();
 
-            $totalPages = ceil($totalArticles / $pageSize);
+        $totalPages = ceil($totalArticles / $pageSize);
 
         return response()->json([
             'products' => $articles,
             'totalPages' => $totalPages,
         ]);
     }
-    public function paginationPaginate(){
+    public function paginationPaginate()
+    {
         $perPage = request()->input('pageSize', 2);
 
         $filterDesignation = request()->input('filtre');
@@ -67,13 +74,14 @@ class ArticleController extends Controller
         $query = Article::with('scategories');
         if ($filterDesignation) {
             $query->where('designation', 'like', '%' . $filterDesignation .
-            '%');
+                '%');
         }
         $articles = $query->paginate($perPage);
 
         return response()->json([
             'products' => $articles->items(),
-            'totalPages' => $articles->lastPage(), ]);
+            'totalPages' => $articles->lastPage(),
+        ]);
     }
 
 }
