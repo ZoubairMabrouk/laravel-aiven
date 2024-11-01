1bb11bc148293dcf6ac6d9acb84aa88ab38a8fb3
ZoubairMabrouk <zoubairmabrouk55@gmail.com>
2024-11-01 20:45:22 +0100

    Vesrion 5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-
+requests">
+<link href="{{ secure_asset('frontend/assets/index-CZpCW9iA.css') }}"
+rel="stylesheet">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
@@ -16,6 +19,9 @@
         </style>
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        <div id="root"></div>
+<script src="{{ secure_asset('frontend/assets/index-CvTbDyNd.js')
+}}"></script>
         <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
             <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" />
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,7 @@ Route::get('/', function () {
 Route::get('/categories', function () {
     return view('categories');
 });
+Route::get('/{any}', function () {
+    return view('welcome');
+    })->where('any', '.*');
 
